e2323b14831abeeed8198ab7cf316b4d3f92fd16
Michael Williamson <mike@zwobble.org>
2009-08-09 20:46:27 +0100

    Add zuice.inspect.get_function_args_spec

--- zuice/inspect.py
+++ zuice/inspect.py
@@ -7,12 +7,20 @@ class Argument(object):
         self.default = default
         
 def get_method_args_spec(function):
+    return _get_args_spec(function, True)
+
+def get_function_args_spec(function):
+    return _get_args_spec(function, False)
+    
+def _get_args_spec(function, remove_first_arg):
     arg_specs = inspect.getargspec(function)
-    arg_names = arg_specs[0][1:]
+    arg_names = arg_specs[0]
+    if remove_first_arg:
+        arg_names = arg_names[1:]
     default_args = arg_specs[3] or []
     number_of_required_args = len(arg_names) - len(default_args)
     has_defaults = ([False] * number_of_required_args) + ([True] * len(default_args))
     defaults = ([None] * number_of_required_args) + list(default_args)
     
     return map(lambda i: Argument(arg_names[i], has_defaults[i], defaults[i]), range(0, len(arg_names)))
-
+    
