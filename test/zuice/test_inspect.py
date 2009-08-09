7eb33fd7d7d5733c87c14ba9e111a40f0e745e92
Michael Williamson <mike@zwobble.org>
2009-08-09 20:40:16 +0100

    Revert "Rename zuice.inspect.get_method_args_spec to get_args_spec"
    This reverts commit 42c52b0dedf15a05a42f58793e263f60c64c3756.

--- test/zuice/test_inspect.py
+++ test/zuice/test_inspect.py
@@ -11,19 +11,22 @@ class SampleObject(object):
 
 class TestInspect(unittest.TestCase):
     def test_names_of_methods_are_retrieved(self):
-        method_spec = zuice.inspect.get_args_spec(SampleObject.foo)
+        method_spec = zuice.inspect.get_method_args_spec(SampleObject.foo)
         self.assertEquals('first', method_spec[0].name)
         self.assertEquals('second', method_spec[1].name)
         self.assertEquals('third', method_spec[2].name)
         
     def test_can_determine_whether_an_argument_has_a_default(self):
-        method_spec = zuice.inspect.get_args_spec(SampleObject.foo)
+        method_spec = zuice.inspect.get_method_args_spec(SampleObject.foo)
         self.assertFalse(method_spec[0].has_default)
         self.assertTrue(method_spec[1].has_default)
         self.assertTrue(method_spec[2].has_default)
         
     def test_can_get_defaults_from_arguments(self):
-        method_spec = zuice.inspect.get_args_spec(SampleObject.foo)
+        method_spec = zuice.inspect.get_method_args_spec(SampleObject.foo)
         self.assertEquals(None, method_spec[0].default)
         self.assertEquals(None, method_spec[1].default)
         self.assertEquals(30, method_spec[2].default)
+    
+    def test_get_method_args_spec_with_no_default_arguments(self):
+        method_spec = zuice.inspect.get_method_args_spec(SampleObject.bar)
