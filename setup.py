db209ca0d36835f0d1a82893ae227f11d317aa13
Michael Williamson <mike@zwobble.org>
2013-09-20 16:02:02 +0100

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='zuice',
-    version='0.2.0',
+    version='0.2.1',
     description='A dependency injection framework for Python',
     author='Michael Williamson',
     author_email='mike@zwobble.org',
