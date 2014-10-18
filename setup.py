e70d6f7bb8a5204b566112b1367e698fc91ea186
Michael Williamson <mike@zwobble.org>
2014-10-18 17:14:55 +0100

    Bump version to 0.3.0-pre1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='zuice',
-    version='0.3.0-dev',
+    version='0.3.0-pre1',
     description='A dependency injection framework for Python',
     long_description=read("README.rst"),
     author='Michael Williamson',
